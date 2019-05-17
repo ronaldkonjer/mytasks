f604d59f80f871098be830feffa0b25a200c425a
Ronald Konjer <ronaldkonjer@gmail.com>
2019-05-17 14:13:19 +0200

    initial commit

---
+++ src/main/java/info/sliceoflife/mytasks/MytasksApplication.java
@@ -0,0 +1,13 @@
+package info.sliceoflife.mytasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytasksApplication.class, args);
+	}
+
+}
